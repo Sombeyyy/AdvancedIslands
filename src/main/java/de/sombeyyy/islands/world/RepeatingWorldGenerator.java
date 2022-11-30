8b745e139259b2e1fd053d1b67889c5c206715e8
Bastian <contact@sombeyyy.de>
2022-11-30 22:56:59 +0100

    Added method to reduce chunk coordinates to values between 0 and 15

--- src/main/java/de/sombeyyy/islands/world/RepeatingWorldGenerator.java
+++ src/main/java/de/sombeyyy/islands/world/RepeatingWorldGenerator.java
@@ -5,6 +5,7 @@ import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.generator.WorldInfo;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
 import java.util.Random;
 
 public class RepeatingWorldGenerator extends ChunkGenerator {
@@ -21,6 +22,11 @@ public class RepeatingWorldGenerator extends ChunkGenerator {
         }
     }
 
+    /**
+     * Generate flat chunk data without noise
+     * @param chunkData the data of the chunk
+     * @return chunk with flat layers
+     */
     private ChunkData generateDefaultChunk(ChunkData chunkData) {
         chunkData.setRegion(0, -64, 0, 15, -61, 15, Material.BEDROCK);
         chunkData.setRegion(0, -60, 0, 15, -48, 15, Material.STONE);
@@ -28,6 +34,47 @@ public class RepeatingWorldGenerator extends ChunkGenerator {
         //y=-32 to y=-25 can contain sand after landscaping
         chunkData.setRegion(0, -32, 0, 15, 0, 15, Material.WATER);
         return chunkData;
+    }
+
+    /**
+     * Manipulate the flat layer chunk so there is some repeating noise
+     * @param chunkData the data of the chunk
+     * @param chunkX x coordinate of the chunk
+     * @param chunkZ z coordinate of the chunk
+     * @return chunk with repeating noisiness
+     */
+    private ChunkData manipulateDefaultChunk(ChunkData chunkData, int chunkX, int chunkZ) {
+        Pair<Byte, Byte> reducedCoords = getReducedChunkCoords(chunkX, chunkZ);
+        return chunkData;
+    }
+
+    /**
+     * Reduces the chunk coordinate to values between 0 and 15
+     * @param chunkX x coordinate of the chunk (west/east)
+     * @param chunkZ z coordinate of the chunk (north/west)
+     * @return values between 0 and 15
+     */
+    private Pair<Byte, Byte> getReducedChunkCoords(int chunkX, int chunkZ) {
+        chunkX = chunkX >> 4;
+        chunkZ = chunkZ >> 4;
+        return new Pair<>((byte) chunkX, (byte) chunkZ);
+    }
+
+    private record Pair<T, K>(T first, K second) {
+
+        @Override
+            public String toString() {
+                return "Pair<" + first + "," + second + ">";
+            }
+
+            @Override
+            public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || getClass() != o.getClass()) return false;
+                Pair<?, ?> pair = (Pair<?, ?>) o;
+                return Objects.equals(first, pair.first) && Objects.equals(second, pair.second);
+            }
+
     }
 
 }
